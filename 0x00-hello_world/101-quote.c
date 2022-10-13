1484b968ad5da0bb6bf2dd366c5a6530e52c9a34
Ayobami <ayobamidele006@gmail.com>
2022-10-13 21:05:23 +0100

    add newfile

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 1
+ */
+int main(void)
+{
+	int length = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+		  "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+		  length);
+	return (1);
+}
