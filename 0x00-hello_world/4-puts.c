58cde9ce81bee93ae8f7ae80fa55bf84f1564242
Ayobami <ayobamidele006@gmail.com>
2022-10-13 19:49:12 +0100

    add new puts c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints out proogramming
+ * Return: All good
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
