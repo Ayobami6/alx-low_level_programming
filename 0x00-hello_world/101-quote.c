ce3e52a751da9d44311170675181f17315bc1209
Ayobami <ayobamidele006@gmail.com>
2022-10-13 21:22:47 +0100

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	int l = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		  59);
