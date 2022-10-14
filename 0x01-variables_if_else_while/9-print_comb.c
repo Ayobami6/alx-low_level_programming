04e9389e3313266d54fda03f4b0fe47ab4b1d6fd
Ayobami <ayobamidele006@gmail.com>
2022-10-14 20:52:30 +0100

    add 0x09 file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all possible combinations fo single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
