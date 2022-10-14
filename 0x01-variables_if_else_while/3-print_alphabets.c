4dbcfc6aeaf138bddc141b6f7dee087b6ba8c3ee
Ayobami <ayobamidele006@gmail.com>
2022-10-14 20:32:35 +0100

    fix 03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 /**
  * main - main block
@@ -10,12 +11,19 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		if (c != 'e' || c != 'q')
-		{
-			putchar(c);
-		}
+		putchar(c);
 		c++;
 	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
 	putchar('\n');
+
 	return (0);
 }
