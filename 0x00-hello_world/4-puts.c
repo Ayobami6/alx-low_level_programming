816ae28f609ecda15cf28d4a5ed876d2c63195b6
Ayobami <ayobamidele006@gmail.com>
2022-10-14 19:31:37 +0100

    add new files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * main - Prints out proogramming
- * Return: All good
- */
+/**                                                                                                                                                             
+* main - Prints out proogramming                                                                                                                                           
+* Return: All good                                                                                                                                                  
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+# alx-low_level_programming
+Variables and conditionals in C
