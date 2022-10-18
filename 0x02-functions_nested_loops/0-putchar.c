3c59f99275882b084ededd79fd68d86b7dc6d2ff
Ayobami <ayobamidele006@gmail.com>
2022-10-18 15:45:49 +0100

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints out _putchar
+ * Return: All good
+ */
+int main(void)
+{
+	char *s = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
