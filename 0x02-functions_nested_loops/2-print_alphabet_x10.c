0f5255ba7d85fc3aeb9946edd373b61882302d9f
Ayobami <ayobamidele006@gmail.com>
2022-10-18 16:45:20 +0100

    tast 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabets in lowercase 10 times
+ * Return: All good
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
