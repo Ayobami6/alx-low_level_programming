2c57f756e8997ebde881a610a73763524f5bb413
Ayobami <ayobamidele006@gmail.com>
2022-10-18 16:54:31 +0100

    task 03

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * Return: 1 if lowercase, 0 otherwise lowercase
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
