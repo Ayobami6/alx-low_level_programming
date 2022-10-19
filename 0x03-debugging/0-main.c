c11b90a90823d5009ab8d55450f567d345b5ff4a
Ayobami <ayobamidele006@gmail.com>
2022-10-19 18:05:04 +0100

    init debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - testing the positive or negative func
+ * Return: All good
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
