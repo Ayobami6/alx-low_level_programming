e5735af8782981314a9939144d5f5eade11d3632
Ayobami <ayobamidele006@gmail.com>
2022-10-20 18:39:24 +0100

    task 08

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
