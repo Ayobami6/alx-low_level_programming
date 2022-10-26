de44d22546a6dc6047045b39edc8c8816da8fcca
Ayobami <ayobamidele006@gmail.com>
2022-10-26 21:30:17 +0100

    add task 0x03

--- 0x06-pointers_arrays_strings/strcat_p.c
+++ 0x06-pointers_arrays_strings/strcat_p.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 
-char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 int main(void)
 {
     char *a;
     char b[50] = "hey";
-    char c[] = "hi";
-    a = _strncat(b, c, 1);
+    char c[] = "peace";
+    a = _strncpy(b, c, 1);
 
     printf("%s\n,", a);
 }
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-    int i, j;
+    int j;
 
-    for (i = 0; dest[i] != '\0'; i++)
-    {
-        ;
-    }
     for (j = 0; j < n && src[j] != '\0'; j++)
     {
-        dest[i] = src[j];
-        i++;
+        dest[j] = src[j];
     }
+    for (; j < n; j++)
+        dest[j] = '\0';
 
-    dest[i] = '\0';
     return dest;
 }
