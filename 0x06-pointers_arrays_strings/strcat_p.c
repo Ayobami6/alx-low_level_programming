f51e8f9ca994ca02cf28a37c2e18aedb580fbfe9
Ayobami <ayobamidele006@gmail.com>
2022-10-26 20:59:31 +0100

    update readme

--- 0x06-pointers_arrays_strings/strcat_p.c
+++ 0x06-pointers_arrays_strings/strcat_p.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
-char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 int main(void)
 {
     char *a;
     char b[50] = "hey";
     char c[] = "hi";
-    a = _strcat(b, c);
+    a = _strncat(b, c, 1);
 
     printf("%s\n,", a);
 }
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
     int i, j;
+
     for (i = 0; dest[i] != '\0'; i++)
     {
         ;
     }
-    for (j = 0; src[j] != '\0'; j++)
+    for (j = 0; j < n && src[j] != '\0'; j++)
     {
         dest[i] = src[j];
         i++;
