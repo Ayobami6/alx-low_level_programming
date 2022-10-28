b076bec41b5d7a456f47f54b50b30e49d05a9257
Ayobami <ayobamidele006@gmail.com>
2022-10-28 11:15:10 +0100

    solution to task 0x08

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a string by rotating for 13char 
+ * @s: string arg
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char* l = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char* r13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j])
+			{
+				s[i] = r13[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
