8f786d07652a510cf6f4810c1fd9da2be96fda8f
Ayobami <ayobamidele006@gmail.com>
2022-10-28 09:22:54 +0100

    add task 0x05

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - convert string to upper
+ * @word: pointer arg 1
+ * Return: String in uppercase
+*/
+
+char *string_toupper(char *word)
+{
+	int i;
+
+	for (i = 0; word[i] != '\0'; i++)
+	{
+		if (word[i] > 96 && word[i] < 123)
+			word[i] -= 32;
+	}
+	return (word);
+}
