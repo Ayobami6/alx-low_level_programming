a5f12e96ef306aba166843eb7b516d2dc3b1b8ac
Ayobami <ayobamidele006@gmail.com>
2022-11-01 10:18:53 +0100

    add task 0x02

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locate character in a string
+ * @s: char array string
+ * @c: char to find
+ * Return: NULL if char not found, or the pointer if found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
+	}
+	if (*s == '\0')
+		return (NULL);
+}
