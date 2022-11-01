894ab76b7f0bccd2cb08ee666af0ead486ef2880
Ayobami <ayobamidele006@gmail.com>
2022-11-01 10:04:44 +0100

    fix betty bug

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-		i++;
-	}
-	return (dest)
+
+	return (dest);
 }
