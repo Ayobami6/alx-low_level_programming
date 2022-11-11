4eb0e683a7c4d2260e612d9ab142e5bc8c825711
Ayobami <ayobamidele006@gmail.com>
2022-11-11 19:10:28 +0100

    fix bug

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,17 +11,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    int *p;
-    unsigned int i;
+	int *p;
+	unsigned int i;
 
-    if (nmemb <= 0 || size <= 0)
-        return (NULL);
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
 
-    p = malloc(nmemb * size);
-    if (p == NULL)
-        return (NULL);
-    for (i = 0; i < nmemb; i++)
-        p[i] = 0;
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
 
-    return (p);
+	return (p);
 }
