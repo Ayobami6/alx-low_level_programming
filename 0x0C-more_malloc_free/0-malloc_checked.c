fe119a4ed6634e8ebd42ceb810c4f7bdd3cfd227
Ayobami <ayobamidele006@gmail.com>
2022-11-11 19:33:15 +0100

    fix bug

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -13,6 +13,7 @@ void *malloc_checked(unsigned int b)
 	void *p;
 
 	p = malloc(b);
+
 	if (p == NULL)
 		exit(98);
 
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,21 +9,21 @@
  */
 int *array_range(int min, int max)
 {
-	int *p;
-	int i, size;
+    int *p;
+    int i, size;
 
-	if (min > max)
-		return (NULL);
+    if (min > max)
+        return (NULL);
 
-	size = max - min + 1;
+    size = max - min + 1;
 
-	p = malloc(sizeof(int) * size);
+    p = malloc(sizeof(int) * size);
 
-	if (p == NULL)
-		return (NULL);
+    if (p == NULL)
+        return (NULL);
 
-	for (i = 0; min <= max; i++)
-		p[i] = min++;
+    for (i = 0; min <= max; i++)
+        p[i] = min++;
 
-	return (p);
+    return (p);
 }
