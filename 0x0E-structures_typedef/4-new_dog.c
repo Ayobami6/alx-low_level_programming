1be569a0993b3e37d1c53f2fcb1ef0ddcb281d6d
Ayobami <ayobamidele006@gmail.com>
2022-11-14 15:59:31 +0100

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,89 +2,48 @@
 #include <stdlib.h>
 
 /**
- * _strlen - return length of a string
- * @s: string
- * Return: a, variable for the string length
- */
-int _strlen(char *s)
-{
-	int a;
-
-	a = 0;
-	if (*s == '\0')
-		return (0);
-
-	a = 1 + _strlen(s + 1);
-
-	return (a);
-}
-/**
- * _strdup - Duplicate a string using malloc
- * @str: string to duplicate
- * Return: Pointer to a the new duped string
- */
-
-char *_strdup(char *str)
-{
-	char *a;
-	int i, j;
-
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	a = malloc(i * sizeof(char) + 1);
-	if (a == NULL)
-		return (NULL);
-
-	for (j = 0; j < i; j++)
-		a[j] = str[j];
-	a[j] = '\0';
-
-	return (a);
-}
-
-/**
  * new_dog - create a new dog
  * @name: char string name
  * @age: int age
  * @owner: char string owner
  * Return: pointer to new dog
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *dogN;
 	int i, j, k;
 	char *n, *o;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	dogN = malloc(sizeof(dog_t));
+	if (dogN == NULL)
 		return (NULL);
-	i = _strlen(name);
-	j = _strlen(owner);
-	n = malloc((i * sizeof(char)) + 1);
 
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+
+	n = malloc(sizeof(char) * i + 1);
 	if (n == NULL)
 	{
-		free(new_dog);
+		free(dogN);
 		return (NULL);
 	}
-
-	o = malloc((i * sizeof(char)) + 1);
+	o = malloc(sizeof(char) * j + 1);
 	if (o == NULL)
 	{
 		free(n);
-		free(new_dog);
+		free(dogN);
 		return (NULL);
 	}
-	n = _strdup(name);
-	o = _strdup(owner);
-	new_dog->name = n;
-	new_dog->age = age;
-	new_dog->owner = o;
+	for (k = 0; k <= i; k++)
+		n[k] = name[k];
+	for (k = 0; k <= j; k++)
+		o[k] = owner[k];
 
-	return (new_dog);
+	dogN->name = n;
+	dogN->age = age;
+	dogN->owner = o;
+
+	return (dogN);
 }
