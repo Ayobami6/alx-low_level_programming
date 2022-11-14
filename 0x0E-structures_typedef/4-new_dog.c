f5f8983288dc3403ea2d54f5bb6fce185810a10c
Ayobami <ayobamidele006@gmail.com>
2022-11-14 15:53:22 +0100

    fix bug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,14 +2,57 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - return length of a string
+ * @s: string
+ * Return: a, variable for the string length
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = 0;
+	if (*s == '\0')
+		return (0);
+
+	a = 1 + _strlen(s + 1);
+
+	return (a);
+}
+/**
+ * _strdup - Duplicate a string using malloc
+ * @str: string to duplicate
+ * Return: Pointer to a the new duped string
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	a = malloc(i * sizeof(char) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		a[j] = str[j];
+	a[j] = '\0';
+
+	return (a);
+}
+
+/**
  * new_dog - create a new dog
  * @name: char string name
  * @age: int age
  * @owner: char string owner
  * Return: pointer to new dog
  */
-int _strlen(char *s);
-char *_strdup(char *str);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -44,51 +87,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = o;
 
 	return (new_dog);
-}
-
-/**
- * _strlen_recursion - return length of a string
- * @s: string
- * Return: a, variable for the string length
- */
-
-int _strlen(char *s)
-{
-	int a;
-
-	a = 0;
-	if (*s == '\0')
-		return (0);
-
-	a = 1 + _strlen(s + 1);
-
-	return (a);
-}
-
-/**
- * _strdup - Duplicate a string using malloc
- * @str: string to duplicate
- * Return: Pointer to a the new duped string
- */
-
-char *_strdup(char *str)
-{
-	char *a;
-	int i, j;
-
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	a = malloc(i * sizeof(char) + 1);
-	if (a == NULL)
-		return (NULL);
-
-	for (j = 0; j < i; j++)
-		a[j] = str[j];
-	a[j] = '\0';
-
-	return (a);
 }
