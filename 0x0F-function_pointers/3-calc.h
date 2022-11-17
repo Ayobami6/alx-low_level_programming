1653d27acead8ec8f162e1852bd473d380dbbe5e
Ayobami <ayobamidele006@gmail.com>
2022-11-17 20:48:08 +0100

    fix bug in h file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,8 @@
+#ifndef CALC_H
+#define CALC_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifndef CALC_H
-#define CALC_H
 
 /**
  * struct op - Struct op
@@ -12,8 +12,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
