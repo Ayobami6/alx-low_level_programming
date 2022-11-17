82b1bbb3021594b0aee59ab9472117b19c458039
Ayobami <ayobamidele006@gmail.com>
2022-11-17 20:23:37 +0100

    test

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (size <= 0 || array == NULL || cmp == NULL)
+	if (size <= 0 && array == NULL && cmp == NULL)
 		return (-1);
 
 	for (int i = 0; i < size; i++)
