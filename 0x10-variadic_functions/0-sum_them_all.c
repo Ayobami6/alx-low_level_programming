db074b4b05668875994fdf8945fdf77217db8065
Ayobami <ayobamidele006@gmail.com>
2022-11-18 17:56:38 +0100

    refactor code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,10 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - return the sum of all its parameters
- * @n: number of extra params
- * Return: total sum
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -13,13 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);
 
-	if (n != 0)
-	{
-		for (i = 0; i < n; i++)
-			sum += va_arg(list, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-		va_end(list);
-	}
+	va_end(list);
 
 	return (sum);
 }
