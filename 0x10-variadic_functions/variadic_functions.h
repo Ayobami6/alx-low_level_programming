14aa4899522fae9a0ab54beb70aa9b7cad4a914e
Ayobami <ayobamidele006@gmail.com>
2022-11-18 17:38:46 +0100

    fix bug

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARADIC_FUNCTION_H
+#define VARADIC_FUNCTION_H
 #include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
