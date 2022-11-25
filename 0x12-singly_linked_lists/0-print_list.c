3301723abbc46b16973bac8496269ca1e580219b
Ayobami <ayobamidele006@gmail.com>
2022-11-25 18:20:57 +0100

    fix bug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
 	size_t node = 0;
 
-	for (tmp = h; tmp != NULL; tmp = tmp->next)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
-			printf("[%u] %s\n", tmp->len, tmp->str);
+			printf("[%d] %s\n", h->len, h->str);
 
+		h = h->next;
 		node++;
 	}
 
