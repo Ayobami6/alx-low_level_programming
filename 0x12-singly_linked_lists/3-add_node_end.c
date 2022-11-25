c2d964b711f49718da6a99e709d20dac2add3d77
Ayobami <ayobamidele006@gmail.com>
2022-11-25 19:13:02 +0100

    solve tast 0x03

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * add_node_end - add a new node at the end of `list_t` list
+ * @head: double pointer to head
+ * @str: string to duplicate into new node
+ * Return: Address of the new element or NULL if failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp;
+	list_t *new_node;
+
+	unsigned int l;
+
+	tmp = *head;
+
+	while (tmp && !tmp->next)
+	{
+		tmp = tmp->next;
+	}
+
+	for (l = 0; str[l] != '\0'; l++)
+		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = l;
+	new_node->str = strdup(str);
+	new_node->next = NULL;
+
+	if (tmp)
+		tmp->next = new_node;
+	else
+		*head = new_node;
+	return (new_node);
+}
