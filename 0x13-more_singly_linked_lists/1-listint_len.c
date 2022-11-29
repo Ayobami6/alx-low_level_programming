f2dd4844f42e295393eb7b3cf2609849b3479cc3
Ayobami <ayobamidele006@gmail.com>
2022-11-29 12:11:52 +0100

    fixed another bug

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * listint_len - print integers in the list
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * add_nodeint - add a new node
