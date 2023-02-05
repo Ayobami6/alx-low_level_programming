75a095e1b8a83162e35bfa064f035c1ffe655856
Ayobami <ayobamidele006@gmail.com>
2023-02-05 15:30:01 +0100

    completed first task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ * Return: hashtable or NULL if failed
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+	unsigned int i;
+
+	if (size == NULL)
+		return (NULL);
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashtable->array == NULL)
+	{
+		free(hashtable);
+		return (NULL);
+	}
+	hashtable->size = size;
+
+	for (i = 0; i < size; i++)
+	{
+		hashtable->array[i] = NULL;
+	}
+	return (hashtable);
+}
