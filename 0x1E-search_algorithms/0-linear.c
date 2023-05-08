cad399c04dc96fbc4991741042b738340e5e7e76
Ayobami <ayobamidele006@gmail.com>
2023-05-08 13:27:19 +0100

    linear search algo added

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * linear_search - searchs for a value in a list
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to search for
+ * Return: index if sucessful, -1 or else
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * Prototypes
+ **/
+
+int linear_search(int *array, size_t size, int value);
