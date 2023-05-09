4e4d4fe24ee5c06da457d231edbdd3ef852db249
Ayobami <ayobamidele006@gmail.com>
2023-05-09 19:34:47 +0100

    added guards to the header files

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,14 @@
+#ifndef SORTING_ALGOS_H
+#define SORTING_ALGOS_H
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 
-/**
- * Prototypes
- **/
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+
+#endif
